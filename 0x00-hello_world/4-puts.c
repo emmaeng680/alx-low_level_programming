ddaa3178e003d2a0802cf4367b7329c81581e811
Emmanuel Oppong <oppongemma73@gmail.com>
2022-06-30 14:43:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main is the entry point
+ * Description: 'the program's description'
+ * Return Always set to 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
