e71ffdf553a3c34848c756882b3e00a9014c4fd4
Emmanuel Oppong <oppongemma73@gmail.com>
2022-07-05 19:06:36 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Return: 0 Success
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
