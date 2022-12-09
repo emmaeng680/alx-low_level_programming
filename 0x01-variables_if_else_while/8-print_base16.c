82446d6a7301d156f376a0e6abb317aea0e041a1
Emmanuel <oppongemma73@gmail.com>
2022-12-09 18:41:28 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'all hexadecimal numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 16 ; i++)
+{
+printf("%x", i);
+
+}
+printf("\n");
+
+
+return (0);
+}
+
