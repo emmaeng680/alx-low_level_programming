3c986196727633d2b13844dbff3f1cdbd6827306
Emmanuel <oppongemma73@gmail.com>
2022-12-09 16:28:39 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print all smallcase except e and q'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alphabet, capitals;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
+
+if (alphabet == 'q' || alphabet == 'e')
+{
+continue;
+}
+
+else
+{
+putchar(alphabet);
+}
+
+}
+
+putchar(10);
+
+
+return (0);
+}
+
