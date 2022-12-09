3996c8a2a4bfcb9ae29c09f8b80f5d9d36610776
Emmanuel <oppongemma73@gmail.com>
2022-12-09 14:23:21 +0000

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Description: 'the program's description'
+ * Description: 'the program's print out whether a number is positive, negative or zero'
  * Return: Always 0 (Success)
  */
 int main(void)
