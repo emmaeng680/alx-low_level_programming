9f5f69ce4c519ed367257decb5a65266bb4c558f
Emmanuel <oppongemma73@gmail.com>
2022-12-09 16:53:05 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print last digit of n with conditions'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int zero;
+
+for (zero = 0 ; zero <= 9 ; zero++)
+{
+printf("%d", zero);
+
+}
+printf("\n");
+
+return (0);
+}
+
