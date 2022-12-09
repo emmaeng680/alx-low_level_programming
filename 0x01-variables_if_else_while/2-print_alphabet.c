8305a919647f08cc7b475a22791da854b8a420f1
Emmanuel <oppongemma73@gmail.com>
2022-12-09 16:02:15 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print last digit of n with conditions'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alphabet;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
+putchar(alphabet);
+
+
+}
+putchar(10);
+
+
+return (0);
+}
+
