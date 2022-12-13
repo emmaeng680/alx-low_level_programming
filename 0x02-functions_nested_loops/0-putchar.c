6998181b2bf75b31ff02ed370138d68a6e43d71e
Emmanuel <oppongemma73@gmail.com>
2022-12-13 09:36:00 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char message[9] = "_putchar";
+printf("%s\n", message);
+
+return (0);
+}
+
