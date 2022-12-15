5a77d77d6792698a5898c21ffb9d9c357c36ee14
Emmanuel <oppongemma73@gmail.com>
2022-12-15 23:54:09 +0000

    Completed

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,23 +6,23 @@
  * Description: 'is random number positive, negative or zero'
  * Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-        int n;
+        int i;
 
         srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
+        i = rand() - RAND_MAX / 2;
+        if (i > 0)
         {
-                printf("%d is positive\n", n);
+                printf("%d is positive\n", i);
         }
-        else if (n == 0)
+        else if (i == 0)
         {
-                printf("%d is zero\n", n);
+                printf("%d is zero\n", i);
         }
         else
         {
-                printf("%d is negative\n", n);
+                printf("%d is negative\n", i);
         }
         return (0);
 }
