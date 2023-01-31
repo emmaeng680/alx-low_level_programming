8b8196c812605ccec88a26ed0c82585eb0662e2c
Emmanuel Oppong <oppongemma73@gmail.com>
2023-01-31 16:52:10 +0000

    Completed

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,8 +8,9 @@
  * Return: numbers of nodes.
  */
 size_t listint_len(const listint_t *h)
- nnodes = 0;
- deint.c  102-free_listint_safe.c   3-add_nodeint_end.c  6-pop_listint.c    9-insert_nodeint.c
+nnodes = 0;
+deint.c  102 - free_listint_safe.c   3 - add_nodeint_end.c
+6 - pop_listint.c    9 - insert_nodeint.c
 
 	while (h != NULL)
 	{
